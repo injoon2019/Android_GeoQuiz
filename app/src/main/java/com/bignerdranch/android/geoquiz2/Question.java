0d6770abb7e1b519204c8c634864d77576bec88a
injun2018 <41927388+injun2018@users.noreply.github.com>
2019-01-12 22:56:14 -0800

    GeoQuiz2

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz2/Question.java
@@ -0,0 +1,28 @@
+package com.bignerdranch.android.geoquiz2;
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int TextResId, boolean AnswerTrue){
+        mTextResId=TextResId;
+        mAnswerTrue=AnswerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
